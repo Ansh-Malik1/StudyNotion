7df8c09ee870945a8204dbd37952128e257fd277
Ansh Malik <anshmalik718@gmail.com>
2024-07-12 18:55:46 +0530

    added mail sending functionality

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mailSender = require('../utils/mailSender')
 
 const otpSchema = new mongoose.Schema({
     email:{
@@ -14,6 +15,25 @@ const otpSchema = new mongoose.Schema({
         default:Date.now(),
         expires:5*60
     }
+})
+
+
+// Function to send email:
+async function sendVerificationMail(email,otp){
+    try{
+        const title = "Verification mail from StudyNotion"
+        const mailResponse = await mailSender(email,title,otp)
+    }
+    catch(error){
+        console.log("Error in sending mail")
+        console.log(error)
+        throw error
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationMail(this.email,this.otp)
+    next()
 })
 
 module.exports = mongoose.model('OTP',otpSchema)
---
+++ server/utils/mailSender.js
@@ -0,0 +1,27 @@
+const nodemailer  =  require('node-mailer')
+
+const mailSender = async(email,title,body)=>{
+    try{
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+        })
+        let info = await transporter.sendMail({
+            from:'StudyNotion || Ansh Malik',
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body}`
+        })
+        console.log(info)
+        return info
+    }
+    catch(error){
+        console.log("Error in mailsender.js")
+        console.log(error)
+    }
+}
+
+module.exports = mailSender
